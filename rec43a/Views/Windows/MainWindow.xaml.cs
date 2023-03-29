7a8a126a957fc2d8da9a0c6501cdf8cb61176efa
Aleksandr90210 <yudin.aleksandrkpc802020@mail.ru>
2023-03-29 13:22:56 +0300

    Добавьте файлы проекта.

---
+++ rec43a/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using rec43a.ViewsModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace rec43a.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+           e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        private void findButton_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as MainWindowViewModel).Summ(int.Parse(numberTextBox.Text));
+
+        }
+    }
+}
---
+++ rec43a/ViewsModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rec43a.ViewsModels.Base;
+
+
+namespace rec43a.ViewsModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private int number;
+        public int Number 
+        { 
+            get { return number; }
+            set { Set(ref number, value); }
+        }
+        
+        public int Summ(int n)
+        {
+            if (n == 0)
+                return 0;
+
+            return Number = n % 10 + Summ(n / 10);
+        }
+    
+    }
+}
